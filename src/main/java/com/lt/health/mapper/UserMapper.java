6f2d27f74d99ca3a922ec402e37fe8622a99cd0f
liteng <liteng2002823@outlook.com>
2022-03-21 22:39:22 +0800

    UserMapper.java

---
+++ src/main/java/com/lt/health/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.lt.health.mapper;
+
+import com.lt.health.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * @author 狂小腾
+ * @description 针对表【user(用户表)】的数据库操作Mapper
+ * @createDate 2022-03-21 22:07:23
+ * @Entity com.lt.health.entity.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
