f64da53480b835a42b32feecfc21c3126df54a86
liteng <liteng2002823@outlook.com>
2022-03-21 22:41:07 +0800

    init

---
+++ src/main/java/com/lt/health/PersonalHealthApplication.java
@@ -0,0 +1,18 @@
+package com.lt.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 项目启动
+ *
+ * @author 狂小腾
+ */
+@SpringBootApplication
+public class PersonalHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalHealthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lt/health/PersonalHealthApplicationTests.java
@@ -0,0 +1,25 @@
+package com.lt.health;
+
+import com.lt.health.entity.User;
+import com.lt.health.service.UserService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+class PersonalHealthApplicationTests {
+
+    @Resource
+    private UserService userService;
+
+    @Test
+    void contextLoads() {
+        List<User> list = userService.list();
+        int size = list.size();
+        Assertions.assertEquals(0, size);
+    }
+
+}
