687d1c98b7906424815e83b146e595c1a15a68c4
liteng <liteng2002823@outlook.com>
2022-03-21 22:38:35 +0800

    MybatisPlusConfig.java

---
+++ src/main/java/com/lt/health/config/MybatisPlusConfig.java
@@ -0,0 +1,29 @@
+package com.lt.health.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * Mybatis-Plus分页插件
+ *
+ * @author 狂小腾
+ */
+@Configuration
+@MapperScan("com.lt.health.mapper")
+public class MybatisPlusConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.H2));
+        return interceptor;
+    }
+}
