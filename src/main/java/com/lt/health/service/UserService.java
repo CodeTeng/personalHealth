90274dfa72de83eab87d19b13e51f6c0232cea29
liteng <liteng2002823@outlook.com>
2022-03-21 22:39:58 +0800

    UserService.java

---
+++ src/main/java/com/lt/health/service/UserService.java
@@ -0,0 +1,13 @@
+package com.lt.health.service;
+
+import com.lt.health.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 狂小腾
+* @description 针对表【user(用户表)】的数据库操作Service
+* @createDate 2022-03-21 22:07:23
+*/
+public interface UserService extends IService<User> {
+
+}
